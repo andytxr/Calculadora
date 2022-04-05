c23cdb22669164ec3439db4f5a141747334f89f3
decosawa <62211442+andytxr@users.noreply.github.com>
2022-04-05 16:17:56 -0300

    Adicionando Switch de Operações e Números

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -7,6 +7,7 @@ class CalcController{
         this._dateEl = document.querySelector("#data");
         this._timeEl = document.querySelector("#hora");
         this._currentDate;
+        this._operation = [];
         this.initialize();
         this.initButtonsEvents();
 
@@ -26,18 +27,73 @@ class CalcController{
         });
     }
 
+    clearAll(){
+        this._operation=[];
+    }
+    clearEntry(){
+        this._operation.pop()
+    }
+    addOperation(value){
+        this._operation.push(value);
+        console.log(this._operation)
+    }
+    setError(){
+        this.displayCalc = "ERROR!";
+    }
+
+    execBtn(value){
+        switch(value){
+
+            case 'ac':
+                this.clearAll();
+                break;
+            case 'ce':
+                this.clearEntry();
+                break;
+            case 'soma':
+
+                break;
+            case 'multiplicacao':
+
+                break;
+            case 'divisao':
+
+                break;
+            case 'subtracao':
+
+                break;
+            case 'porcento':
+
+                break;
+            case 'igual':
+
+                break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+            default:
+                this.setError();
+                break;
+        }
+    };
+
     initButtonsEvents(){
 
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
         buttons.forEach((btn, index) => {
 
-            this.addEventListenerAll(btn,'click drag', e =>{
+            this.addEventListenerAll(btn,'click drag mouseover', e =>{
                 console.log(btn.className.baseVal.replace("btn-",""));
-            })
-
-            this.addEventListenerAll(btn, 'mouseover mouseup mousedown', e =>{
-                btn.style.cursor="pointer";
             })
         })
 
