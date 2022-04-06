7c6baeaf6ea0542dff2787e5479d2d31d0e354ab
decosawa <62211442+andytxr@users.noreply.github.com>
2022-04-06 18:02:01 -0300

    Funcionalidade de Copia e Cola

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -10,21 +10,54 @@ class CalcController{
         this._operation = [];
         this._lastOperator='';
         this._lastNumber='';
+        this._audioOnOff=false;
+        this._audio = new Audio('click.mp3');
+
         this.initialize();
         this.initButtonsEvents();
         this.initKeyboard();
+        
 
     }
 
-    //Inicialização
+    //Inicialização e funcionalidades
 
     initialize(){
 
         setInterval(() => {
+
             this.setDisplayDateTime();
+
         }, 1000);
+
         this.setLastNumberToDisplay();
-       
+        this.pasteFromClipboard();
+
+        document.querySelectorAll('.btn-ac').forEach(btn =>{
+
+            btn.addEventListener('dblclick', e=>{
+            
+                this.toggleAudio();
+
+            });
+        });
+    }
+
+    toggleAudio(){
+
+        this._audioOnOff=!this._audioOnOff;
+
+        //O áudio está desligado, toda vez que ele for "passado" por ele, ele se torna o inverso:
+        //OFF=!OFF (!=NÃO) > OFF=ON > ON=!ON > OFF=!OFF
+
+    }
+    playAudio(){
+
+        if(this._audioOnOff){
+
+            this._audio.play();
+
+        }
     }
 
     initKeyboard(){
@@ -44,15 +77,25 @@ class CalcController{
                 case '9':
                     this.addOperation(parseInt(e.key));
                     break;
+
                 case 'Escape':
                     this.clearAll();
                     break;
+
                 case 'Backspace':
                     this.clearEntry();
                     break;
+
                 case 'Enter':
                     this.calc();
                     break;
+
+                case 'c':
+                    if(e.ctrlKey){
+                        this.copyToClipboard();
+                    }
+                    break;
+                
                 case '+':
                 case '-':
                 case '*':
@@ -78,11 +121,15 @@ class CalcController{
         buttons.forEach((btn, index) => {
 
             this.addEventListenerAll(btn,'click drag', e =>{
+
                 let textBtn = btn.className.baseVal.replace("btn-", "");
                 this.execBtn(textBtn);
+
             })
             this.addEventListenerAll(btn,'mouseover mouseup mousedown', e => {
+
                 btn.style.cursor="pointer";
+
             })
         });
 
@@ -96,7 +143,28 @@ class CalcController{
         });
     }
 
-    //Limpar tudo e limpar última entrada
+    copyToClipboard(){
+
+        let input = document.createElement('input');
+        input.value=this.displayCalc;
+
+        document.body.appendChild(input);
+        input.select();
+        document.execCommand("Copy");
+        input.remove();
+
+    }
+
+    pasteFromClipboard(){
+
+        document.addEventListener('paste', e=>{
+
+            let text = e.clipboardData.getData('Text');
+            this.displayCalc=parseFloat(text);
+
+        });
+
+    }
 
     clearAll(){
 
@@ -121,7 +189,9 @@ class CalcController{
 
         this._operation.push(value);
         if (this._operation.length>3){
+
             this.calc();
+
         }
 
     }
@@ -137,29 +207,40 @@ class CalcController{
         this._lastOperator = this.getLastItem();
         
         if(this._operation.lengt<3){
+
             let firstItem=this._operation[0];
             this._operation=[firstItem,this._lastOperator,this._lastNumber];
+
         }
 
         else if(this._operation.length>3){
+
             last = this._operation.pop();
             this._lastNumber = this.getResult();
+
         }
         else if(this._operation.length==3){
+
             this._lastNumber=this.getLastItem(false);
+
         }
 
         // Calculando porcentagem
         let res=this.getResult();
         if (last == '%'){
+
             res = res/100;
             this._operation[res];
+
         }
         else{
+
             this._operation=[res];
 
             if(last){
+
                 this._operation.push(last);
+
             }
         }
 
@@ -182,6 +263,7 @@ class CalcController{
     setLastOperation(value){
 
         this._operation[this._operation.length-1]=value;
+
     }
 
     getLastItem(isOperator=true){
@@ -197,14 +279,17 @@ class CalcController{
     
             }
         if(!lastItem){
+
             lastItem=(isOperator) ? this._lastOperator : this._lastNumber;
 
             //lastItem=(isOperator) < Condição
             //? < Então se a Condição for verdadeira faça X coisa
             //: < Se a Condição não for verdadeira faça X coisa
+
         }
 
         return lastItem;
+
     }
 
     addOperation(value){
@@ -212,9 +297,12 @@ class CalcController{
         if(isNaN(this.getLastOperation())){
 
             //Strings 
+
             if(this.isOperator(value)){
+
                 //Trocar o operador
                 this.setLastOperation(value);
+
             }else{
 
                 this.pushOperation(value);
@@ -227,13 +315,16 @@ class CalcController{
             //Numbers
 
             if(this.isOperator(value)){
+
                 this.pushOperation(value);
+
             }
             else{
 
                 let newValue = this.getLastOperation().toString()+value.toString();
                 this.setLastOperation(newValue);
                 this.setLastNumberToDisplay(); //Update no display
+
             }
 
         }
@@ -243,13 +334,19 @@ class CalcController{
         let lastOp = this.getLastOperation();
 
         if(typeof lastOp === 'string' && lastOp.split('').indexOf('.') > -1){
+
             return;
+
         }
 
         if(this.isOperator(lastOp) || !lastOp){
+
             this.pushOperation('0.');
+
         }else{
+
             this.setLastOperation(lastOp.toString() + '.');
+
         }
     }
 
@@ -261,35 +358,46 @@ class CalcController{
 
     execBtn(value){
 
+        this.playAudio();
+
         switch(value){
 
             case 'ac':
                 this.clearAll();
                 break;
+
             case 'ce':
                 this.clearEntry();
                 break;
+
             case 'soma':
                 this.addOperation('+');
                 break;
+
             case 'multiplicacao':
                 this.addOperation('*');
                 break;
+
             case 'divisao':
                 this.addOperation('/');
                 break;
+
             case 'subtracao':
                 this.addOperation('-');
                 break;
+
             case 'porcento':
                 this.addOperation('%');
                 break;
+
             case 'igual':
                 this.calc();
                 break;
+
             case 'ponto':
                 this.addDot('.');
                 break;
+
             case '0':
             case '1':
             case '2':
@@ -302,6 +410,7 @@ class CalcController{
             case '9':
                 this.addOperation(parseInt(value));
                 break;
+
             default:
                 this.setError();
                 break;
@@ -326,10 +435,14 @@ class CalcController{
         let lastNumber=this.getLastItem(false);
 
         if(!lastNumber){
+
             lastNumber=0;
+
         }
-        if(lastNumber.toString().length>5){
-            lastNumber = lastNumber.toString().substr(0, 5);
+        if(lastNumber.toString().length>10){
+
+            lastNumber = lastNumber.toString().substr(0, 10);
+
         }
 
         this.displayCalc=lastNumber;
@@ -337,28 +450,44 @@ class CalcController{
     }
     
     get displayTime(){
+
         return this._timeEl.innerHTML;
+
     }
     set displayTime(value){
+
         return this._timeEl.innerHTML=value;
+
     }
     get displayDate(){
+
         return this._dateEl.innerHTML;
+
     }
     set displayDate(value){
+
         return this._dateEl.innerHTML=value;
+
     }
     get displayCalc(){
+
         return this._displayCalcEl.innerHTML;
+
     }
     set displayCalc(value){
+
         this._displayCalcEl.innerHTML=value;
+
     }
     get currentDate(){
+
         return new Date();
+
     }
     set currentDate(value){
+
         this._currentDate=value;
+
     }
 
 }
